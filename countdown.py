3a78ad7247ab59b77d543529b179cc080c52f55b
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-14 22:54:10 -0500

    python-p3-countdown-to-midnight-lab
    python-p3-countdown-to-midnight-lab

--- countdown.py
+++ countdown.py
@@ -1,5 +1,16 @@
+import time
+
+
 def countdown(number):
     while number > 0:
         print(f"{number} SECOND(S)!")
+        number -= 1
+    print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        time.sleep(1)  # Pause for one second
         number -= 1
     print("HAPPY NEW YEAR!")
